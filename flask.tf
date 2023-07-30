021f38db1b2111d872f0b4cf3b45ff69bcbaf2ea
Roey_Gihan <133655565+Gihan4@users.noreply.github.com>
2023-07-30 16:07:52 +0300

    Create flask.tf

---
+++ flask.tf
@@ -0,0 +1,94 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a new key pair for the EC2 instance
+resource "aws_key_pair" "terra_key" {
+  key_name   = "terra_key"
+  public_key = file("/home/gihanroey/.ssh/id_rsa.pub")  # Path to your new public key
+}
+
+# Create an AWS EC2 instance
+resource "aws_instance" "testing" {
+  ami           = "ami-0f34c5ae932e6f0e4"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.terra_key.key_name  # Use the key_name from the created key pair
+
+  tags = {
+    Name = "Example Instance"
+  }
+
+  vpc_security_group_ids = [aws_security_group.web_server_sg.id]  # Attach the security group to the instance
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("/home/gihanroey/.ssh/id_rsa")  # Path to your private key file
+    host        = aws_instance.testing.public_ip
+  }
+
+  # Install Docker on the EC2 instance
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install docker -y",
+      "sudo service docker start",
+      "sudo usermod -aG docker ec2-user"  # Add the current user to the docker group to run docker commands without sudo
+    ]
+  }
+
+  # Pull and run the Docker image for your Flask project
+  provisioner "remote-exec" {
+    inline = [
+      "sudo docker login -u Gihan4 -p Ro123Ey123G4",  # Login to Docker Hub
+      "sudo docker pull gihan4/flask:latest",      # Pull the Docker image
+      "sudo docker run -d -p 5000:5000 gihan4/flask:latest"  # Run the Docker container on port 5000
+    ]
+  }
+}
+
+# Create a security group that allows ports 80, 443, 22, and 5000, with outbound all traffic
+resource "aws_security_group" "web_server_sg" {
+  name_prefix = "example-security-group"
+
+  # Inbound rules
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound rule (allow all traffic)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # -1 represents all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "test_ip" {
+  value = aws_instance.testing.public_ip
+}
