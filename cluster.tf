3b615e89c56479bef2673b0e0bd3f73ce32bd051
Roey_Gihan <133655565+Gihan4@users.noreply.github.com>
2023-08-13 16:36:26 +0300

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -5,7 +5,18 @@ provider "google" {
 }
 
 resource "google_container_cluster" "the_cluster" {
-  name               = "autopilot-cluster"
+  name               = "autopilot-cluster-production"
+  location           = "us-central1" 
+  enable_autopilot   = true
+
+  # Specify the default release channel for Autopilot mode
+  release_channel {
+    channel = "REGULAR"
+  }
+}
+
+resource "google_container_cluster" "second_cluster" {
+  name               = "autopilot-cluster-test"
   location           = "us-central1" 
   enable_autopilot   = true
 
